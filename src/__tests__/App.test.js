b805ee9a8d2d7ed09b008dc37e0236debc6589a2
Anshuman <anshumanhota1@gmail.com>
2026-02-20 23:38:54 +0530

    Adding files

--- src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-// src/__tests__/App.test.js
-import { render, screen } from '@testing-library/react';
-import App from '../App';  // <-- use '../App' because test is in __tests__ folder
-
-test('renders Hotstar Clone heading', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Subscribe to Watch/i);
-  expect(linkElement).toBeInTheDocument();
-});
