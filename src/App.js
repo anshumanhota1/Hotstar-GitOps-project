5610b0d87ed1ae0092c5b7e3a9a8f0935d28a4d6
Anshuman Hota <76252762+anshumanhota1@users.noreply.github.com>
2026-02-20 23:09:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import './App.css';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
@@ -11,56 +12,54 @@ import requests from './request';
 
 function App() {
   return (
-    <>
-   <div className='rel'>
-   <div className="nnn">
-          <Nav/>
-        </div>
-     
-      <div className="aaa">
-        
-  
-  <div className='first'>
-    
-          <NavBar/>
-  </div>
-  
-   <div className='second'>
-  
-    <div className='stick'>
-      
-     <Banner/>
-    </div>
-    <div className='lg'>
-    <img src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2744/1602744-t-f24fc98823fa" alt="" />
-            <h3>2023 . 2h 54m . 4 languages . <span>U/A 16+</span> </h3>
-            <p>In a crime-infested town, Kannan bhai and his gang are the reigning powers. To combat this reign and seek revenge, Inspector </p>
+    <div className="app-container">
+      {/* Navigation */}
+      <header>
+        <Nav />
+        <NavBar />
+      </header>
+
+      {/* Main Banner Section */}
+      <main>
+        <section className="banner-section">
+          <Banner />
+          <div className="featured-movie">
+            <img
+              src="https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2744/1602744-t-f24fc98823fa"
+              alt="Featured Movie"
+            />
+            <h3>
+              2023 . 2h 54m . 4 languages . <span>U/A 16+</span>
+            </h3>
+            <p>
+              In a crime-infested town, Kannan bhai and his gang are the reigning
+              powers. To combat this reign and seek revenge, Inspector ...
+            </p>
             <h3>Action | Drama | Thriller | Drugs</h3>
-            <button className='btn'>Subscribe to Watch</button>
-            <button className='bb btn'>+</button>
-  
-  
-  
+            <div className="buttons">
+              <button className="btn">Subscribe to Watch</button>
+              <button className="btn add-btn">+</button>
+            </div>
+          </div>
+        </section>
+
+        {/* Rows and Categories */}
+        <section className="movies-section">
+          <Row title="Latest releases" fetchUrl={requests.fetchActionMovies} />
+          <Row title="Free-Newly Added" fetchUrl={requests.fetchComedyMovies} />
+          <Row title="Disney Movies" fetchUrl={requests.fetchDocumentaries} />
+          <Platforms />
+          <Language />
+          <Genre />
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+          <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+          <Row title="Popular Movies" fetchUrl={requests.fetchDoc} />
+        </section>
+      </main>
+
+      {/* Footer */}
+      <Footer />
     </div>
-    <div className='z'>
-            
-      <Row title="Latest releases" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Free-Newly Added" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Disney Movies" fetchUrl={requests.fetchDocumentaries}/>
-      <Platforms/>
-      <Language/>
-      <Genre/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Populer Movies" fetchUrl={requests.fetchDoc}/>
-      <Footer/>
-    </div>
-    
-    
-   </div>
-      </div>
-   </div>
-    </>
   );
 }
 
